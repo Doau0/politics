7ae704ad9fbbfeb7b67e318f84f31fea46944966
iantashiro <65839626+iantashiro@users.noreply.github.com>
2020-05-24 00:26:11 -0300

    Update political_advisor.py

--- political_advisor.py
+++ political_advisor.py
@@ -1,3 +1,12 @@
+# um bolsominion é uma criatura que nasceu sem um cérebro
+# e é, portanto, incapaz de pensar. São ovelhas facilmente
+# controláveis e estúpidas, muitas vezes homofóbicas e sempre
+# apresentam um déficit de inteligência muito alto.
+def bolsominion(hasbrain):
+    if hasbrain == False:
+        return True;
+
+
 #rule 1
 if bolsominion == True:
     sentence = death by hanging;
